ef8d8f193cfe2f9a61742e2a2f0dcc862d73ec32
Matej Sestak <sestamat@fel.cvut.cz>
2020-11-03 21:06:14 +0100

    Bet min/max odd

--- src/evaluate.py
+++ src/evaluate.py
@@ -9,7 +9,7 @@ from environment import Environment
 dataset = pd.read_csv('../data/training_data.csv', parse_dates=['Date', 'Open'])
 model = Model()
 env = Environment(dataset, model, init_bankroll=1000., min_bet=5., max_bet=100.)
-evaluation = env.run(start=pd.to_datetime('2005-07-01'))
+evaluation = env.run(start=pd.to_datetime('2005-07-01'), end=pd.to_datetime('2006-06-30'))
 
 print(f'Final bankroll: {env.bankroll:.2f}')
 
--- src/model.py
+++ src/model.py
@@ -1,14 +1,26 @@
 import numpy as np
 import pandas as pd
+from typing import List, Dict
 
 
 class Model:
+    def __init__(self):
+        self.bankroll: float = 0.0
+        self.odds_cols = ['OddsH', 'OddsD', 'OddsA']
+        self.score_cols = ['HSC', 'ASC']
+        self.res_cols = ['H', 'D', 'A']
 
-
-    def place_bets(self, opps, summary, inc):
-        N = len(opps)
-        min_bet = summary.iloc[0].to_dict()['Min_bet']
-        bets = np.zeros((N,3))
-        bets[np.arange(N), np.random.choice([0,1,2], N)] = min_bet 
-        return pd.DataFrame(data=bets, columns=['BetH', 'BetD', 'BetA'], index=opps.index)
+    def place_bets(self, opps: pd.DataFrame, summary: pd.DataFrame, inc: pd.DataFrame):
+        _summary = summary.iloc[0].to_dict()
+        self.bankroll = _summary['Bankroll']
+        min_bet = _summary['Min_bet']
+        max_bet = _summary['Max_bet']
+        date = _summary['Date']
+        today_games = opps[opps['Date'] == date]
+        N = len(today_games)
+        odds = np.argmax(today_games[self.odds_cols].to_numpy(), axis=1)
+        bets = np.zeros((N, 3))
+        bets[np.arange(N), odds] = min_bet
+        print('Opportunities: {}'.format(N))
+        return pd.DataFrame(data=bets, columns=['BetH', 'BetD', 'BetA'], index=today_games.index)
 
